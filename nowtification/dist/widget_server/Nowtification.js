e763f099c5ddb81b0837aef6cf0069f64819782d
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-10-19 11:51:30 -0700

    Nowtificatiion server implemented

--- nowtification/dist/widget_server/Nowtification.js
+++ nowtification/dist/widget_server/Nowtification.js
@@ -1,28 +1,28 @@
-(function() {
+(function () {
 
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */
+    /* populate the 'data' object */
+    /* e.g., data.table = $sp.getValue('table'); */
 
-  	return;
     var myuserid = gs.getUserID();
-    var myuser = gs.getUser();
     var mygroups = gs.getUser().getMyGroups().toArray().join(',');
-    var query = "u_incident.u_business_service.u_l2_support_groupIN"+mygroups+"^ORu_incident.u_business_service.u_support_infraIN"+mygroups+"^ORu_usersLIKE"+myuserid;
-
-	var notificationRead = myuser.getPreference('sevNotificationRead') || "null";
 
 
-	var gr = new GlideRecord("u_severity_incident_management_notification");
-    gr.addQuery('u_type', 'push');
-    gr.addEncodedQuery(query);
-    gr.query();
-	var notificationsToDisplay = [];
-	var n;
-	while(gr.next()) {
-		n = {};
-		n.message = gr.getValue('u_message');
-		notificationsToDisplay.push(n);
-	}
+    if (options.table && options.query) {
+        options.query.replace('$myuserid', myuserid);
+        options.query.replace('$mygroups', mygroups);
 
-	data.notifications = notificationsToDisplay;
+        var gr = new GlideRecord(options.table);
+        gr.addEncodedQuery(options.query);
+        gr.query();
+        var notificationsToDisplay = [];
+        var n;
+        while (gr.next()) {
+            n = {};
+            $sp.getRecordDisplayValues(n, gr, options.fields);
+            notificationsToDisplay.push(n);
+        }
+
+        data.notifications = notificationsToDisplay;
+    }
+
 })();
